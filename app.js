47c23028ce73d9c1d8c059bc2ab39ea4094a7d55
Juuso <juusomakisalo959@gmail.com>
2022-09-05 12:33:57 +0300

    Added deletion and check for adding items to the list

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 (function () {
 'use strict';
+
 angular.module('myApp', [])
-.controller('myCtrl', function ($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
-    $scope.test = "test 1";
+.controller('myCtrl', function ($scope){
+
+$scope.products = ["Milk", "Bread", "Cheese"];
+$scope.addItem = function () {
+$scope.errortext = "";
+
+if (!$scope.addMe) {return;}
+  
+if ($scope.products.indexOf($scope.addMe) == -1) {
+    $scope.products.push($scope.addMe);
+  } else {
+    $scope.errortext = "The item is already in your shopping list.";
+  }
+}
+
+$scope.removeItem = function (x) {
+  $scope.errortext = "";
+  $scope.products.splice(x, 1);
+}
+
 });
 })();
