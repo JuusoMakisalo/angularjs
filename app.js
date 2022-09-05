8ac8b0e482c45e724a5a0f69ae906a9ee83765c8
Juuso <juusomakisalo959@gmail.com>
2022-09-05 14:20:05 +0300

    Changed to database version with external listing htm page.

--- app.js
+++ app.js
@@ -2,26 +2,11 @@
 'use strict';
 
 angular.module('myApp', [])
-.controller('myCtrl', function ($scope){
+.controller('myCtrl', function ($scope, $http){
 
-$scope.products = ["Milk", "Bread", "Cheese"];
-
-$scope.addItem = function () {
-$scope.errortext = "";
-
-if (!$scope.addMe) {return;}
-  
-if ($scope.products.indexOf($scope.addMe) == -1) {
-    $scope.products.push($scope.addMe);
-  } else {
-    $scope.errortext = "The item is already in your shopping list.";
-  }
-}
-
-$scope.removeItem = function (x) {
-  $scope.errortext = "";
-  $scope.products.splice(x, 1);
-}
+  $http.get("https://www.w3schools.com/angular/customers_mysql.php")
+   .then(function (response) {$scope.names = response.data.records;}
+   );
 
 });
 })();
